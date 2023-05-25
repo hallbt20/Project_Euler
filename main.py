ba5cc0197e3e7de90bcc9d6be98f049cf4649249
hallbt20 <62180184+hallbt20@users.noreply.github.com>
2023-05-25 09:33:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,7 @@ from new import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    x = [0, 0, 0, 0, 0]
-    counter = 0
-    while x[4] != 1:
-        x[0] = 200 - 2*x[1] - 5*x[2] - 10*x[3]
-        if x[0] >= 0:
-            print(x)
-            counter += 1
-        carry_over(x)
+    x = [200, 0, 0, 0, 0]
+    while x[2] != 3:
+        x = next_number(x)
         print(x)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
