2f1940b8c4ef6b7396e9c0e953e76697af9d2081
hallbt20 <62180184+hallbt20@users.noreply.github.com>
2023-09-05 15:26:17 -0500

    Add files via upload

---
+++ euler_75.py
@@ -0,0 +1,41 @@
+from math import isqrt
+from math import gcd
+from math import floor
+import numpy as np
+
+dictionary = {}
+
+test = []
+
+for n in [num for num in range(1, 1000) if num % 2 == 1]:
+    bound = (-n + isqrt(pow(n, 2) + 6000000)) // 2
+    for m in [num for num in range(n + 2, bound) if num % 2 == 1 and gcd(num, n) == 1]:
+        L = pow(m, 2) + m * n
+        if L in test:
+            continue
+        test.append(L)
+        k = 1
+        factor = L * k
+        while factor <= 1500000:
+            if factor in dictionary:
+                dictionary[factor] += 1
+            else:
+                dictionary[factor] = 1
+            k += 1
+            factor = L * k
+
+counter = 0
+
+for key in dictionary:
+    if dictionary[key] == 1:
+        counter += 1
+
+print(counter)
+
+print(len(test))
+
+print(len(set(test)))
+        #a = m * n
+        #b = (pow(m, 2) - pow(n, 2)) // 2
+        #c = (pow(m, 2) + pow(n, 2)) // 2
+        #print(a, b, c, sum([a, b, c]))
